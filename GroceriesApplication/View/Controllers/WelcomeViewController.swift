629d38fa75b93bd58169230d36ca147759abfff4
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-01-14 12:21:27 +0300

    Welcome View - Created

--- GroceriesApplication/View/Controllers/WelcomeViewController.swift
+++ GroceriesApplication/View/Controllers/WelcomeViewController.swift
@@ -50,6 +50,50 @@ class WelcomeView: UIView {
         return imageView
     }()
     
+    private lazy var logoView: UIImageView = {
+        let imageView = UIImageView(image: Images.Welcome.logo)
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 42, weight: .bold)
+        label.text = "Welcome to our Store"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        label.text = "Get your groceries in as fast as one hour"
+        label.textColor = .description
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var startButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .primary
+        button.setTitle("Get Started", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        button.layer.cornerRadius = 12
+        button.tintColor = .white
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(didTapStartButton), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func didTapStartButton(){
+        
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -68,7 +112,7 @@ class WelcomeView: UIView {
 
 extension WelcomeView {
     private func setupView(){
-        addSubViews(backgroundImage)
+        addSubViews(backgroundImage, logoView, titleLabel, descriptionLabel, startButton)
     }
     
     private func style(){
@@ -77,5 +121,19 @@ extension WelcomeView {
     private func layout() {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundImage.pinToEdges(of: self)
+        
+        NSLayoutConstraint.activate([
+            logoView.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 20),
+            logoView.bottomAnchor.constraint(equalTo: titleLabel.topAnchor, constant: -40),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 80),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -80),
+            titleLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: -18),
+            descriptionLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            descriptionLabel.bottomAnchor.constraint(equalTo: startButton.topAnchor, constant: -40),
+            startButton.heightAnchor.constraint(equalToConstant: 67),
+            startButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 30),
+            startButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30),
+            startButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -90)
+        ])
     }
 }
